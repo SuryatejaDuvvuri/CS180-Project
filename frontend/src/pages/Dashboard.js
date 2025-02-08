129a2865cc0971120e745e17e30bc2441185ee4e
rraje008 <rraje008@ucr.edu>
2025-02-08 12:10:24 -0800

    Created simple login UI to test if working or not

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Dashboard() {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Welcome to the Dashboard 🎉</h1>
+      </div>
+    );
+  }
+  
