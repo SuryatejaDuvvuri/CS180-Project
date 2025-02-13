a5990bda4d6aeb8bfb719a58d5767ec66be0b580
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2025-02-12 17:18:23 -0800

    Add frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SignUp from './Signup';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import ProjectCreation from './ProjectCreation';
+import ProjectManagement from './ProjectManagement';
 
 function App() {
   const [name, setName] = useState('');
@@ -19,84 +21,9 @@ function App() {
  
   return (
     <div className="App">
-      <SignUp />
-      <br/>
-
-      <h1>New Project</h1>
-      <form onSubmit={(e) => {e.preventDefault(); alert("Working")} } className = "form-container">
-
-        <label className="form-label">Project Image: 
-          <input type = "file" onChange={(e) => setImage(e.target.files[0])} className="form-input"/>
-          {image && (
-            <div className="file-info">
-              <strong>Selected:</strong> {image.name}
-            </div>
-          )}
-        </label>
-
-        <div className="form-label">
-          <span>Color:</span>
-          <div className="color-row">
-            {colorOptions.map((c) => (
-              <div 
-                key={c}
-                onClick={() => setColor(c)}
-                className={`color-circle ${color === c ? 'selected' : ''}`}
-                style={{ backgroundColor: c }}
-              />
-            ))}
-          </div>
-        </div>
-
-
-
-        <label name = "name" className="form-label"> 
-          Project Name: <input type = "text" value = {name} className="form-input" onChange={(e) => setName(e.target.value)}/>
-        </label>
-        <br/>
-        <label className="form-label">
-          <span>Project Description:</span>
-          <textarea
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-            rows={4}
-            className="form-textarea"
-          />
-        </label>
-        <label>Project Duration:  
-        <DatePicker
-          selectsRange={true}
-          startDate={startDate}
-          endDate={endDate}
-          onChange={(update) => {
-            setRange(update);
-          }}
-          isClearable={true}
-        />
-        </label>
-        <label>Number of people: <input type="number" value = {val} onChange = {(e) => setVal(e.target.value)} className="form-input"/></label>
-        <label>Categories: 
-          <datalist>
-            <option value = "Computer Science"/>
-            <option value = "Medicine"/>
-            <option value = "Filmmaking"/>
-            <option value = "Art"/>
-            <option value = "Psychology"/>
-            <option value = "History"/>
-          </datalist> </label>
-        <label>Location: <input type = "text" value = {location} onChange = {(e) => setLocation(e.target.value)}/> </label>
-        <label className="form-label">Weekly Time Commitment (hours): 
-          <input 
-            type="number" 
-            min="1" 
-            max="168" 
-            value={weeklyHours} 
-            onChange={(e) => setWeeklyHours(e.target.value)}
-          />
-        </label>
-        <br/>        
-        <input type = "submit" value = "Submit"/>
-      </form>
+      {/* <SignUp />
+      <ProjectCreation/> */}
+      <ProjectManagement/>
     </div>
  
   );
