95474df1ee9c6c1a97eb803fc9842d7e70bc7e88
Strickvax <strickvax@gmail.com>
2025-02-19 16:44:03 -0800

    Add UserProfile and Home components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ApplicationForm from './ApplicationForm';
+import UserProfile from './UserProfile';
+import Home from './Home';
 
 function App() {
   return (
